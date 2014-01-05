5ccc61209490bbc39f3024759f02e4a456a28e55
Gerhard Lausser <gerhard.lausser@consol.de>
2014-01-06 00:25:45 +0100

    init

---
+++ plugins-scripts/WuT/Device.pm
@@ -0,0 +1,63 @@
+package WuT::Device;
+our @ISA = qw(GLPlugin::SNMP);
+
+use strict;
+use IO::File;
+use File::Basename;
+use Digest::MD5  qw(md5_hex);
+use Errno;
+use AutoLoader;
+our $AUTOLOAD;
+
+use constant { OK => 0, WARNING => 1, CRITICAL => 2, UNKNOWN => 3 };
+
+{
+  our $mode = undef;
+  our $plugin = undef;
+  our $blacklist = undef;
+  our $session = undef;
+  our $rawdata = {};
+  our $info = [];
+  our $extendedinfo = [];
+  our $summary = [];
+  our $statefilesdir = '/var/tmp/check_ups_health';
+  our $oidtrace = [];
+  our $uptime = 0;
+}
+
+sub new {
+  my $class = shift;
+  my %params = @_;
+  my $self = {
+    productname => 'unknown',
+  };
+  bless $self, $class;
+  if (! ($self->opts->hostname || $self->opts->snmpwalk)) {
+    $self->add_message(UNKNOWN, 'either specify a hostname or a snmpwalk file');
+  } else {
+    $self->check_snmp_and_model();
+    if (! $self->check_messages()) {
+      if ($self->opts->verbose && $self->opts->verbose) {
+        printf "I am a %s\n", $self->{productname};
+      }
+      if ($self->implements_mib('WebGraph-8xThermometer-MIB')) {
+        bless $self, 'WuT::WebioAn8Graph';
+        $self->debug('using WuT::WebioAn8Graph');
+      } else {
+        if (my $class = $self->discover_suitable_class()) {
+          bless $self, $class;
+          $self->debug('using '.$class);
+        } elsif ($self->mode =~ /device::uptime/) {
+          bless $self, 'GLPlugin::SNMP';
+        } else {
+          $self->add_message(UNKNOWN, 'the device did not implement the mibs this plugin is asking for');
+          $self->add_message(UNKNOWN,
+              sprintf('unknown device%s', $self->{productname} eq 'unknown' ?
+                  '' : '('.$self->{productname}.')'));
+        }
+      }
+    }
+  }
+  return $self;
+}
+
---
+++ plugins-scripts/WuT/WebioAn8Graph.pm
@@ -0,0 +1,166 @@
+package WuT::WebioAn8Graph;
+our @ISA = qw(WuT::Device);
+
+use strict;
+use constant { OK => 0, WARNING => 1, CRITICAL => 2, UNKNOWN => 3 };
+
+sub init {
+  my $self = shift;
+  if ($self->mode =~ /device::sensor::status/) {
+    $self->analyze_sensor_subsystem();
+    $self->check_sensor_subsystem();
+  } else {
+    $self->no_such_mode();
+  }
+}
+
+sub analyze_sensor_subsystem {
+  my $self = shift;
+  $self->{components}->{sensor_subsystem} =
+      WuT::WebioAn8Graph::SensorSubsystem->new();
+}
+
+
+package WuT::WebioAn8Graph::SensorSubsystem;
+our @ISA = qw(GLPlugin::Item WuT::WebioAn8Graph);
+
+use strict;
+use constant { OK => 0, WARNING => 1, CRITICAL => 2, UNKNOWN => 3 };
+
+sub init {
+  my $self = shift;
+  $self->get_snmp_objects("WebGraph-8xThermometer-MIB", qw(wtWebioAn8GraphDiagErrorCount));
+  $self->get_snmp_objects("WebGraph-8xThermometer-MIB", qw(wtWebioAn8GraphSensors));
+  $self->get_snmp_objects("WebGraph-8xThermometer-MIB", qw(wtWebioAn8GraphAlarmCount));
+  $self->get_snmp_objects("WebGraph-8xThermometer-MIB", qw(wtWebioAn8GraphPorts));
+  $self->get_snmp_tables("WebGraph-8xThermometer-MIB", [
+      ["sensors", "wtWebioAn8GraphBinaryTempValueTable", "WuT::WebioAn8Graph::SensorSubsystem::Sensor"],
+      ["alarms", "wtWebioAn8GraphAlarmTable", "WuT::WebioAn8Graph::SensorSubsystem::Alarm"],
+      ["alarmsf", "wtWebioAn8GraphAlarmIfTable", "WuT::WebioAn8Graph::SensorSubsystem::AlarmIf"],
+      ["ports", "wtWebioAn8GraphPortTable", "WuT::WebioAn8Graph::SensorSubsystem::Port"],
+  ]);
+  foreach my $sensor (@{$self->{sensors}}) {
+    $sensor->{wtWebioAn8GraphBinaryTempValue} /= 10;
+    $sensor->{alarms} = [];
+    foreach my $alarm (@{$self->{alarms}}) {
+      if ($alarm->belongs_to() eq $sensor->{flat_indices}) {
+        push(@{$sensor->{alarms}}, $alarm);
+      }
+    }
+    foreach my $port (@{$self->{ports}}) {
+      if ($port->{flat_indices} eq $sensor->{flat_indices}) {
+        $sensor->{wtWebioAn8GraphPortName} = $port->{wtWebioAn8GraphPortName};
+        if ($sensor->{wtWebioAn8GraphPortName} =~ /^0x/) {
+          $sensor->{wtWebioAn8GraphPortName} =~ s/\s//g;
+          $sensor->{wtWebioAn8GraphPortName} =~ s/0x(([0-9a-f][0-9a-f])+)/pack('H*', $1)/ie;
+        }
+      }
+    }
+  }
+}
+
+sub check {
+  my $self = shift;
+  if ($self->{wtWebioAn8GraphDiagErrorCount}) {
+    $self->add_message(CRITICAL,
+        sprintf "diag error count is %d", $self->{wtWebioAn8GraphDiagErrorCount});
+  }
+  foreach (@{$self->{sensors}}) {
+    $_->check();
+  }
+}
+
+sub dump {
+  my $self = shift;
+  printf "[SENSORS]\n";
+  foreach (qw(wtWebioAn8GraphSensors wtWebioAn8GraphAlarmCount)) {
+    printf "%s: %s\n", $_, $self->{$_};
+  }
+  foreach (@{$self->{sensors}}) {
+    $_->dump();
+  }
+  foreach (@{$self->{alarms}}) {
+    $_->dump();
+  }
+  foreach (@{$self->{ports}}) {
+    $_->dump();
+  }
+}
+
+
+package WuT::WebioAn8Graph::SensorSubsystem::Sensor;
+our @ISA = qw(GLPlugin::TableItem);
+
+use strict;
+use constant { OK => 0, WARNING => 1, CRITICAL => 2, UNKNOWN => 3 };
+
+sub check {
+  my $self = shift;
+  if (scalar(@{$self->{alarms}})) {
+    foreach my $alarm (@{$self->{alarms}}) {
+      if ("" ne $alarm->{wtWebioAn8GraphAlarmMin} && $alarm->{wtWebioAn8GraphAlarmMin} > $self->{wtWebioAn8GraphBinaryTempValue}) {
+        $self->add_message(CRITICAL, sprintf "temperature %s is too low: %s < %s (%s)", 
+            $self->{wtWebioAn8GraphPortName},
+            $self->{wtWebioAn8GraphBinaryTempValue},
+            $alarm->{wtWebioAn8GraphAlarmMin},
+            $alarm->{wtWebioAn8GraphAlarmMailText});
+      } elsif ("" ne $alarm->{wtWebioAn8GraphAlarmMax} && $alarm->{wtWebioAn8GraphAlarmMax} < $self->{wtWebioAn8GraphBinaryTempValue}) {
+        $self->add_message(CRITICAL, sprintf "temperature %s is too high: %s > %s (%s)", 
+            $self->{wtWebioAn8GraphPortName},
+            $alarm->{wtWebioAn8GraphAlarmMax},
+            $self->{wtWebioAn8GraphBinaryTempValue},
+            $alarm->{wtWebioAn8GraphAlarmMailText});
+      } else {
+        $self->add_message(OK, sprintf "temperature %s is in range: %s [%s...%s]",
+            $self->{wtWebioAn8GraphPortName},
+            $self->{wtWebioAn8GraphBinaryTempValue},
+            defined $alarm->{wtWebioAn8GraphAlarmMin} ? $alarm->{wtWebioAn8GraphAlarmMin} : "-",
+            defined $alarm->{wtWebioAn8GraphAlarmMax} ? $alarm->{wtWebioAn8GraphAlarmMax} : "-");
+      }
+      $self->add_perfdata(
+          label => "temp_".$self->{wtWebioAn8GraphPortName},
+          value => $self->{wtWebioAn8GraphBinaryTempValue},
+          min => $alarm->{wtWebioAn8GraphAlarmMin},
+          max => $alarm->{wtWebioAn8GraphAlarmMax},
+      );
+    }
+  }
+}
+
+package WuT::WebioAn8Graph::SensorSubsystem::Alarm;
+our @ISA = qw(GLPlugin::TableItem);
+
+sub belongs_to {
+  my $self = shift;
+  my $trigger = $self->{wtWebioAn8GraphAlarmTrigger};
+  $trigger =~ s/\s//g;
+  if (oct($trigger) & oct("0b00000000000000000000000000000001")) {
+    return 1;
+  } elsif (oct($trigger) & oct("0b00000000000000000000000000000010")) {
+    return 2;
+  } elsif (oct($trigger) & oct("0b00000000000000000000000000000100")) {
+    return 3;
+  } elsif (oct($trigger) & oct("0b00000000000000000000000000001000")) {
+    return 4;
+  } elsif (oct($trigger) & oct("0b00000000000000000000000000010000")) {
+    return 5;
+  } elsif (oct($trigger) & oct("0b00000000000000000000000000100000")) {
+    return 6;
+  } elsif (oct($trigger) & oct("0b00000000000000000000000001000000")) {
+    return 7;
+  } elsif (oct($trigger) & oct("0b00000000000000000000000010000000")) {
+    return 8;
+  } else {
+    return 0;
+  }
+}
+
+package WuT::WebioAn8Graph::SensorSubsystem::AlarmIf;
+our @ISA = qw(GLPlugin::TableItem);
+
+
+package WuT::WebioAn8Graph::SensorSubsystem::Port;
+our @ISA = qw(GLPlugin::TableItem);
+
+
+
