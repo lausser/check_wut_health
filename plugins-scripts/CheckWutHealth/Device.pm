960ebe4e870017b98a5616f88c5e3a982e4866f4
Gerhard Lausser <gerhard.lausser@consol.de>
2024-07-08 21:22:07 +0200

    improve rittallcp dx alarm handling

--- plugins-scripts/CheckWutHealth/Device.pm
+++ plugins-scripts/CheckWutHealth/Device.pm
@@ -13,8 +13,42 @@ sub classify {
         printf "I am a %s\n", $self->{productname};
       }
       my($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
+      $year += 1900;
+      $year = $year % 100;
+      $mon += 1;
+
+      $Monitoring::GLPlugin::SNMP::MibsAndOids::origin->{'CAREL-WHATSBEHIND-MIB'} = {
+        url => '',
+        name => 'PcoWeb with something behind it',
+      };
+      $Monitoring::GLPlugin::SNMP::MibsAndOids::mibs_and_oids->{'CAREL-WHATSBEHIND-MIB'} = {
+        # pd pcoweb-dumb, nix angepasst, einfach nur Klima drangehaengt
+        # pst pcoweb-with-sysoid-trick, sysoid hingefaked (s.u. Librenms)
+        # lcp clearly-rittal-lcp-agentCode, echte oids von Rittal
+        'pd-agentCode' => '1.3.6.1.4.1.9839.1.2',
+        'pst-agentCode' => '1.3.6.1.4.1.9839.2606.1.2',
+        'lcp-agentCode' => '1.3.6.1.4.1.2606.21.1.2',
+        'pd-current-year' => '1.3.6.1.4.1.9839.2.1.3.12',
+        'pst-current-year' => '1.3.6.1.4.1.9839.2606.2.1.3.12',
+        'lcp-current-year' => '1.3.6.1.4.1.2606.21.2.1.3.12',
+        'pd-current-month' => '1.3.6.1.4.1.9839.2.1.3.10',
+        'pst-current-month' => '1.3.6.1.4.1.9839.2606.2.1.3.10',
+        'lcp-current-month' => '1.3.6.1.4.1.2606.21.2.1.3.10',
+      };
       if ($self->opts->mode =~ /^my-/) {
         $self->load_my_extension();
+      } elsif (defined $self->get_snmp_object("CAREL-WHATSBEHIND-MIB", "pd-agentCode") and $self->get_snmp_object("CAREL-WHATSBEHIND-MIB", "pd-agentCode") == 2 and defined $self->get_snmp_object("CAREL-WHATSBEHIND-MIB", "pd-current-year") and $self->get_snmp_object("CAREL-WHATSBEHIND-MIB", "pd-current-year") == $year and defined $self->get_snmp_object("CAREL-WHATSBEHIND-MIB", "pd-current-month") and $self->get_snmp_object("CAREL-WHATSBEHIND-MIB", "pd-current-month") == $mon) {
+        foreach my $oid (keys %{$Monitoring::GLPlugin::SNMP::MibsAndOids::mibs_and_oids->{'RITTAL-LCP-DX-MIB'}}) {
+          $Monitoring::GLPlugin::SNMP::MibsAndOids::mibs_and_oids->{'RITTAL-LCP-DX-MIB'}->{$oid} =~ s/1\.3\.6\.1\.4\.1\.2606\.21/1.3.6.1.4.1.9839/g;
+        }
+        $self->rebless('CheckWutHealth::Rittal::LCPDX');
+      } elsif (defined $self->get_snmp_object("CAREL-WHATSBEHIND-MIB", "pst-agentCode") and $self->get_snmp_object("CAREL-WHATSBEHIND-MIB", "pst-agentCode") == 2 and defined $self->get_snmp_object("CAREL-WHATSBEHIND-MIB", "pst-current-year") and $self->get_snmp_object("CAREL-WHATSBEHIND-MIB", "pst-current-year") == $year and defined $self->get_snmp_object("CAREL-WHATSBEHIND-MIB", "pst-current-month") and $self->get_snmp_object("CAREL-WHATSBEHIND-MIB", "pst-current-month") == $mon) {
+        foreach my $oid (keys %{$Monitoring::GLPlugin::SNMP::MibsAndOids::mibs_and_oids->{'RITTAL-LCP-DX-MIB'}}) {
+          $Monitoring::GLPlugin::SNMP::MibsAndOids::mibs_and_oids->{'RITTAL-LCP-DX-MIB'}->{$oid} =~ s/1\.3\.6\.1\.4\.1\.2606\.21/1.3.6.1.4.1.9839.2606/g;
+        }
+        $self->rebless('CheckWutHealth::Rittal::LCPDX');
+      } elsif (defined $self->get_snmp_object("CAREL-WHATSBEHIND-MIB", "lcp-agentCode") and $self->get_snmp_object("CAREL-WHATSBEHIND-MIB", "lcp-agentCode") == 2 and defined $self->get_snmp_object("CAREL-WHATSBEHIND-MIB", "lcp-current-year") and $self->get_snmp_object("CAREL-WHATSBEHIND-MIB", "lcp-current-year") == $year and defined $self->get_snmp_object("CAREL-WHATSBEHIND-MIB", "lcp-current-month") and $self->get_snmp_object("CAREL-WHATSBEHIND-MIB", "lcp-current-month") == $mon) {
+        $self->rebless('CheckWutHealth::Rittal::LCPDX');
       } elsif ($self->implements_mib('WebGraph-8xThermometer-MIB')) {
         $self->rebless('CheckWutHealth::WebioAn8Graph');
       } elsif ($self->implements_mib('WEBGRAPH-THERMO-HYGROMETER-MIB')) {
@@ -43,10 +77,6 @@ sub classify {
         $self->rebless('CheckWutHealth::Papouch');
       } elsif ($self->implements_mib('ENVIROMUX5D')) {
         $self->rebless('CheckWutHealth::NTI');
-      } elsif ($self->implements_mib("KELVIN-PCOWEB-LCP-DX-MIB") and 1) {
-        #$self->get_snmp_object("KELVIN-PCOWEB-LCP-DX-MIB", "current-year") and
-        #$self->get_snmp_object("KELVIN-PCOWEB-LCP-DX-MIB", "current-year") == $year - 100) {
-        $self->rebless('CheckWutHealth::Carel::pCOWeb');
       } elsif ($self->implements_mib('RITTAL-LCP-DX-MIB') || $self->get_snmp_object('RITTAL-LCP-DX-MIB', 'setpoint-lcp')) {
         $self->rebless('CheckWutHealth::Rittal::LCPDX');
         # Rumtrickserei, Mib alleine reicht nicht. Wegen:
@@ -87,3 +117,112 @@ sub init {
     $self->no_such_mode();
   }
 }
+
+__END__
+      #
+      # Carel PCOWeb ist so ein Mini-Webserverdings, an welches alle
+      # moeglichen Hersteller sich dranhaengen. Oder besser andersrum:
+      # In den riesigsten Klimaanlagen (https://www.rittal.com/lt-en/products/PG0168KLIMA1/PGR1951KLIMA1/PG1023KLIMA1/PRO33639?variantId=3313480 z.b.)
+      # wird so ein Pco als Netzwerk-Frontend eingebaut.
+      # Hintenraus spricht das Dings ein sog. HVAC-Protokoll, welches Modbus,
+      # Ethernet, Serial und weiss der Geier was sein kann.
+      # Pco faengt mit .1.3.6.1.4.1.9839 an
+      # Wie es aussieht nehmen die die Rittal-OIDs und ersetzen einfach
+      # das 2606 in 1.3.6.1.4.1.2606.* durch 9839. Das ist natuerlich
+      # konfigurierbar, so dass 1.3.6.1.4.1.9839.2606.* genauso moeglich ist.
+      # sysObjID ist auch konfigurierbar, d.h. hierauf ist auch kein Verlass,
+      # das ist allenfalls ein Hinweis.
+      # Weil Librenms vorschlaegt, Enterprise OID anzupassen...ich lese gerade
+      # in https://www.carel.com/documents/10191/0/+030220966/12c36f92-bfa6-4418-97bf-ecc21acc4f37?version=1.1
+      # "Enterprise OID (required for sending the TRAP message):
+      #
+      # Das Dings hat OIDs für
+      # digital group 2.1.1.[1..207]
+      # integer group 2.1.2.[1..207]
+      # analog group 2.1.3.[1..207]
+      # Die Bedeutung haengt dann davon ab, welcher Hersteller hintendran
+      # verbaut wurde. Je nachdem sind auch nur 1..n in Verwendung.
+      # 2.1.1.23 ist bei einem Rittal LCP z.b. ein General Alarm, bei
+      # CAREL-ug40cdz.MIB, UG40 Close Control Uniflair device
+      # digitalObjects.23 = Alarm: Room High Humidity
+      # UNCDZ.MIB, UNIFLAIR UNCDZ device
+      # digitalObjects 23 = Alarm: High Pressure 2
+      # rittal-LCP-DX.mib, Rittal's LCP DX units
+      # digitalObjects 23 = General Alarm Contact
+      # kelvin-pCOWeb-Chiller.MIB, New MIB Chiller
+      # digitalObjects 23 = Overload fan 4 circuit 1 alarm
+      # DataAire-dap4-al-MIB.mib
+      # digitalObjects 23 = No water flow alarm
+      #
+      #
+# MIBS/CAREL-RITTAL-LCP-3311-MIB   # selbstgebaut , achtung 2 versionen
+# MIBS/Rittal-LCP-DX.MIB
+
+# KELVIN-pCOWeb-Chiller-MIB -> KELVIN-pCOWeb-Chiller-MIB
+#  201309131602Z
+#  This is the MIB module for the kelvin-pCOWeb-Chiller device
+#  New MIB Chiller
+#  digitalObjects 1.3.6.1.4.1.9839.2.1.1
+
+# pCOWeb_LCP-DX_RimatriX_2014_rev01.mib -> KELVIN-pCOWeb-LCP-DX-MIB
+#  201309131602Z
+#  This is the MIB module for the kelvin-pCOWeb-LCP-DX device
+#  DESCRIPTION "New MIB-LCP"
+#  digitalObjects 1.3.6.1.4.1.9839.2.1.1
+#  current-year 1.3.6.1.4.1.9839.2.1.3.12
+
+# Rittal-LCP-DX.MIB -> Rittal-LCP-DX-MIB
+#  201312011000Z
+#  This is the official MIB module for the Rittal's LCP DX units
+#  201309221200Z
+#  A new Rittal OID 1.3.6.1.4.1.2606.21 has been associated to LCP DX family
+#  First release, made by Kelvin: New MIB-LCP
+#  digitalObjects 1.3.6.1.4.1.2606.21.2.1.1
+#  current-year 1.3.6.1.4.1.2606.21.2.1.3.12
+
+.1.3.6.1.2.1.1.2.0 = OID: .1.3.6.1.4.1.9839.2606
+.1.3.6.1.4.1.9839.1.1.0 = INTEGER: 4
+.1.3.6.1.4.1.9839.1.2.0 = INTEGER: 2
+.1.3.6.1.4.1.9839.1.3.1.1.0 = STRING: "alarm fired"
+.1.3.6.1.4.1.9839.1.3.1.2.0 = STRING: "alarm reentered"
+.1.3.6.1.4.1.9839.2.0.10.1.0 = INTEGER: 2
+.1.3.6.1.4.1.9839.2.0.11.1.0 = INTEGER: 0
+
+.1.3.6.1.2.1.1.2.0 = OID: .1.3.6.1.4.1.9839.2606.1
+.1.3.6.1.4.1.9839.2606.1.1.0 = INTEGER: 4
+.1.3.6.1.4.1.9839.2606.1.2.0 = INTEGER: 2
+.1.3.6.1.4.1.9839.2606.1.3.1.1.0 = STRING: "alarm fired"
+.1.3.6.1.4.1.9839.2606.1.3.1.2.0 = STRING: "alarm reentered"
+.1.3.6.1.4.1.9839.2606.2.0.10.1.0 = INTEGER: 2
+.1.3.6.1.4.1.9839.2606.2.0.11.1.0 = INTEGER: 0
+
+.1.3.6.1.2.1.1.2.0 = OID: .1.3.6.1.4.1.8072.3.2.10
+.1.3.6.1.4.1.2606.21.1.1.0 = INTEGER: 4
+.1.3.6.1.4.1.2606.21.1.2.0 = INTEGER: 2
+.1.3.6.1.4.1.2606.21.1.3.1.1.0 = STRING: "alarm fired"
+.1.3.6.1.4.1.2606.21.1.3.1.2.0 = STRING: "alarm reentered"
+.1.3.6.1.4.1.2606.21.2.0.10.1.0 = INTEGER: 2
+.1.3.6.1.4.1.2606.21.2.0.11.1.0 = INTEGER: 0
+ziemlich klar, das ist Rittal-LCP-DX-MIB
+
+.1.3.6.1.2.1.1.2.0 = OID: .1.3.6.1.4.1.8072.3.2.10
+.1.3.6.1.4.1.9839.1.1.0 = INTEGER: 4
+.1.3.6.1.4.1.9839.1.2.0 = INTEGER: 2
+.1.3.6.1.4.1.9839.1.3.1.1.0 = STRING: "alarm fired"
+.1.3.6.1.4.1.9839.1.3.1.2.0 = STRING: "alarm reentered"
+.1.3.6.1.4.1.9839.2.0.10.1.0 = INTEGER: 2
+.1.3.6.1.4.1.9839.2.0.11.1.0 = INTEGER: 0
+
+Basis rausfinden, also alle
+1.3.6.1.4.1.9839
+1.3.6.1.4.1.9839.2606
+1.3.6.1.4.1.2606.21
+Dazu Pseudo-MIBS mit
+.1.3.6.1.4.1.9839.1.2.0
+.1.3.6.1.4.1.9839.2606.1.2.0
+.1.3.6.1.4.1.2606.21.1.2.0
+Der Wert muss 2 sein, Code of the Agent. 2=pCOWeb
+Dann <basis>.2.1.3.12, muss das Jahr sein. Wenns passt, dann LCP
+Und <basis>.2.1.3.10 ist der Monat
+(Wenns ein Rittal Chiller ist, der hat bei 2.1.3.12 irgendein Gateway und
+bei .10 eine Netzmaske 8-16-24-32. Kombi aus beiden sollte eindeutig sein)
--- plugins-scripts/CheckWutHealth/Rittal/LCPDX/Component/EnvironmentalSubsystem.pm
+++ plugins-scripts/CheckWutHealth/Rittal/LCPDX/Component/EnvironmentalSubsystem.pm
@@ -5,30 +5,104 @@ use strict;
 sub init {
   my ($self) = @_;
   # alarm-output DESCRIPTION "General Alarm Contact" (ist 1, aber gui sagt ok)
-  @{$self->{infooids}} = (qw(
-    comp-on
-  ));
-  @{$self->{alarmoids}} = (qw(
-    comp-overload high-pressure alarm-inverter alarm-off-line
-    comp-on0 comp-on1 comp-on2 bms-res-alarm 
-    al-envelope al-start-fail-lock mal-start-failure-msk 
-    mal-discharge-ht mal-dp-startup mal-dp-lubrification-oil 
-    mal-b1 alarm-server-in-temp1 alarm-server-in-temp2 alarm-server-in-temp3 
-    mal-b5 alarm-server-out-temp1 alarm-server-out-temp2 alarm-server-out-temp3 
-    alarm-comp-discharge-temp alarm-comp-suction-temp 
-    alarm-comp-discharge-pressure alarm-comp-suction-pressure 
-  ));
-  $self->get_snmp_objects("RITTAL-LCP-DX-MIB", (@{$self->{alarmoids}}, @{$self->{infooids}}));
+  $self->{digital_descriptions} = {
+    "comp-overload" => "Drive/Compressor Overload",
+    "high-pressure" => "High Pressure Switch Alarm",
+    "remote-on-off" => "Remote ON/OFF", # off (0), on (1)"
+    "alarm-inverter" => "General Inverter Alarm",
+    "alarm-off-line" => "Off-Line inverter Alarm", # driveAlarm Power+ drive off
+#    "comp-on" => "Compressor On", # inverterOnOff Inverter On/Off
+    "alarm-output" => "General Alarm Contact",
+    "al-envelope" => "Envelope Alarm", # Compressor forced off working out envelope
+    "al-start-fail-lock" => "Start failure lock Alarm",
+    "mal-discharge-ht" => "Dicharge HT Alarm", # Maximum discharge temperature has been reached
+    "mal-dp-startup" => "Delta pressure Start-Up compressor too high",
+    "mal-dp-lubrification-oil" => "Delta pressure Lubrification oil too low",
+    "alarm-server-in-temp1" => "Probe B2 Alarm (Broken or Disconnected)",
+    "alarm-server-in-temp2" => "Probe B3 Alarm (Broken or Disconnected)",
+    "alarm-server-in-temp3" => "Probe B4 Alarm (Broken or Disconnected)",
+    "alarm-server-out-temp1" => "Probe B6 Alarm (Broken or Disconnected)",
+    "alarm-server-out-temp2" => "Probe B7 Alarm (Broken or Disconnected)",
+    "alarm-server-out-temp3" => "Probe B8 Alarm (Broken or Disconnected)",
+    "alarm-comp-discharge-temp" => "Probe B9 Alarm (Broken or Disconnected)",
+    "alarm-comp-suction-temp" => "Probe B10 Alarm (Broken or Disconnected)",
+    "alarm-comp-discharge-pressure" => "Probe B11 Alarm (Broken or Disconnected)",
+    "alarm-comp-suction-pressure" => "Probe B12 Alarm (Broken or Disconnected)",
+  };
+  $self->get_snmp_objects("RITTAL-LCP-DX-MIB", keys %{$self->{digital_descriptions}});
+  $self->{analog_descriptions} = {
+    "error-code" => "Current Error Code",
+  };
 }
 
 sub check {
   my ($self) = @_;
-  foreach (grep { defined $self->{$_} } @{$self->{alarmoids}}) {
-    $self->add_info(sprintf 'alarm %s is %sset', $_, $self->{$_} ? "" : "not");
-    if ($self->{$_}) {
+  my $errors = 0;
+  foreach (grep { defined $self->{$_} } keys %{$self->{digital_descriptions}}) {
+    $self->add_info($self->{digital_descriptions}->{$_}." is ".$self->{$_});
+    if ($_ eq "alarm-output") {
+      next;
+      # schon mal bestaetigt, dass das auf 1 war, in der GUI aber alles ok war
+      # Laut einer schwindligen CAREL-RITTAL-LCP-3311-MIB.mib ist alarm=0,ok=1
+      # Andere wiederum behauptet glatt das Gegenteil.
+      # Hat keine Aussagekraft. Mehrere abgefragt:
+      # .1.3.6.1.4.1.9839.2.1.1.23.0 = INTEGER  0
+      # .1.3.6.1.4.1.9839.2606.2.1.1.23.0 = INTEGER  0
+      # .1.3.6.1.4.1.2606.21.2.1.1.23.0 = INTEGER  1
+      # .1.3.6.1.4.1.2606.21.2.1.1.23.0 = INTEGER  1
+      # .1.3.6.1.4.1.2606.21.2.1.1.23.0 = INTEGER  1
+      # .1.3.6.1.4.1.2606.21.2.1.1.23.0 = INTEGER  1
+      # .1.3.6.1.4.1.9839.2.1.1.23.0 = INTEGER  1
+      # Ich sehe keinen Weg, irgendeine Unterscheidung zwischen den Geraeten
+      # zu treffen. Hoffen wir darauf, dass ein alarm-output andere Alarme
+      # nach sich zieht.
       $self->add_critical();
+      $errors++;
+    } elsif ($self->{$_}) {
+      $self->add_critical();
+      $errors++;
     }
   }
-  delete $self->{alarmoids};
+  if (! $errors and $self->{"error-code"}) {
+    # https://github.com/epiecs/carel-pco-mibs?tab=readme-ov-file
+    # https://www.rittal.com/de-de/products/PG0800ITINFRA1/PGR1951ITINFRA1/PG1023ITINFRA1/PRO34169?variantId=3311410
+    # Stellt sich raus, der Dreck ist seit drei Jahren abgekuendigt!!!!
+    $self->add_info({
+      0 => "OK",
+      2 => "Probe B2 faulty or disconnected",
+      3 => "Probe B3 faulty or disconnected",
+      4 => "Probe B4 faulty or disconnected",
+      5 => "Probe B5 faulty or disconnected",
+      6 => "Probe B6 faulty or disconnected",
+      7 => "Probe B7 faulty or disconnected",
+      8 => "Probe B8 faulty or disconnected",
+      9 => "Probe B9 faulty or disconnected",
+      10 => "Probe B10 faulty or disconnected",
+      11 => "Probe B11 faulty or disconnected",
+      12 => "Probe B12 faulty or disconnected",
+      13 => "High pressure",
+      14 => "High pressure compressor 1 by transducer",
+      15 => "Low pressure compressor/compressors by transducer",
+      16 => "Compressor 1 overload or inverter alarm",
+      17 => "Envelope alarm zone",
+      18 => "Compressor start failure",
+      19 => "High discharge gas temperature",
+      20 => "Low pressure differential (insufficient lubrication)",
+      21 => "Fan overload",
+      22 => "Sensor failure",
+      23 => "EEV motor error",
+      24 => "Low superheat",
+      25 => "Low suction temperature",
+      26 => "Low evaporation temperature",
+      27 => "High evaporation temperature",
+      28 => "High condensing temperature",
+      29 => "Driver offline",
+      30 => "Power+ offline",
+      31 => "Power+ Generic Alarm",
+      32 => "Unexpected inverter stop",
+      33 => "Max temperature (warning)",
+    }->{$self->{"error-code"}});
+    $self->add_critical();
+  }
 }
 
